bcc07379a5026d6ed9a458f68fc10ef6b465ee59
Leopold Lemmermann <leopold@lemmermann.com>
2022-03-01 10:37:35 +0100

    Initial Commit

---
+++ Portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by Leopold Lemmermann on 01.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Leopold Lemmermann on 01.03.22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
