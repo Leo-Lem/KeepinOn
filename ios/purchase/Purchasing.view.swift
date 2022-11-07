2a01478b15da11b2120cf09c67cc38a4f3f740b8
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-07 20:17:05 +0100

    Implemented the purchase view.

--- ios/purchase/Purchasing.view.swift
+++ ios/purchase/Purchasing.view.swift
@@ -4,7 +4,28 @@ import SwiftUI
 
 struct PurchasingView: View {
   var body: some View {
-    Text("Purchasing View")
+    VStack {
+      Spacer()
+
+      switch vm.purchaseState {
+      case .loading:
+        ProgressView("IAP_LOADING")
+      case let .loaded(purchase):
+        PurchasingView.PurchaseView(purchase: purchase) { purchase in
+          Task(priority: .userInitiated) { await vm.purchase(purchase) }
+        }
+      case .success:
+        Text("IAP_SUCCESSFUL")
+      case .pending:
+        Text("IAP_PENDING")
+      case .failed:
+        Text("IAP_ERROR")
+      }
+
+      Spacer()
+    }
+    .padding()
+    .preferred(style: SheetViewStyle(size: .fraction(0.4)))
   }
 
   @StateObject private var vm: ViewModel
