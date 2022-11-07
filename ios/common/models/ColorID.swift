309c467aa32cfcb104320c5414e93c75c5ca1f74
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-07 20:00:05 +0100

    Added basic models.

---
+++ ios/common/models/ColorID.swift
@@ -0,0 +1,7 @@
+//	Created by Leopold Lemmermann on 27.10.22.
+
+import Foundation
+
+enum ColorID: String, CaseIterable, Hashable, Codable {
+  case pink, purple, red, orange, gold, green, teal, lightBlue, darkBlue, midnight, darkGray, gray
+}
---
+++ ios/common/models/Project.swift
@@ -0,0 +1,34 @@
+//	Created by Leopold Lemmermann on 07.10.22.
+
+import Foundation
+
+struct Project: Identifiable, Hashable, Codable {
+  let id: UUID, timestamp: Date
+
+  var title: String,
+      details: String,
+      isClosed: Bool,
+      colorID: ColorID,
+      reminder: Date?,
+      items: [Item]
+
+  init(
+    id: UUID = UUID(),
+    timestamp: Date = .now,
+    title: String = "",
+    details: String = "",
+    isClosed: Bool = false,
+    colorID: ColorID = .darkBlue,
+    reminder: Date? = nil,
+    items: [Item] = []
+  ) {
+    self.id = id
+    self.timestamp = timestamp
+    self.title = title
+    self.details = details
+    self.isClosed = isClosed
+    self.colorID = colorID
+    self.reminder = reminder
+    self.items = items
+  }
+}
