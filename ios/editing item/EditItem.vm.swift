5e19b94651b77d4273bde790be5bb1bc69055731
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-07 20:16:11 +0100

    Implemented the edit item view.

--- ios/editing item/EditItem.vm.swift
+++ ios/editing item/EditItem.vm.swift
@@ -6,10 +6,48 @@ extension EditItemView {
   @MainActor final class ViewModel: KeepinOn.ViewModel {
     private let item: Item
 
+    @Published var title: String
+    @Published var details: String
+    @Published var priority: Item.Priority
+    @Published private(set) var isDone: Bool
+
     init(item: Item, appState: AppState) {
       self.item = item
 
+      title = item.title
+      details = item.details
+      isDone = item.isDone
+      priority = item.priority
+
       super.init(appState: appState)
     }
+  }
+}
+
+extension EditItemView.ViewModel {
+  func setIsDone(_ new: Bool) {
+    isDone = new
+    updateItem()
+    Task(priority: .userInitiated) {
+      if new {
+        await printError {
+          try await awardService.itemsCompleted(1)
+        }
+      }
+    }
+  }
+
+  func updateItem() {
+    var item = item
+    item.title = title
+    item.details = details
+    item.isDone = isDone
+    item.priority = priority
+
+    do {
+      try privateDatabaseService.insert(item)
+    } catch { print(error.localizedDescription) }
+
+    routingService.dismiss()
   }
 }
