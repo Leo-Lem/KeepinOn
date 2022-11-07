7b746588722bfa0117d9fa32597ff1b42d27563a
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-07 20:15:54 +0100

    Implemented the edit project view.

---
+++ ios/common/ui/colorID/ColorID.asset.swift
@@ -0,0 +1,31 @@
+//	Created by Leopold Lemmermann on 27.10.22.
+
+import SwiftUI
+
+extension ColorID {
+  var asset: String {
+    switch self {
+    case .pink: return "Pink"
+    case .purple: return "Purple"
+    case .red: return "Red"
+    case .orange: return "Orange"
+    case .gold: return "Gold"
+    case .green: return "Green"
+    case .teal: return "Teal"
+    case .lightBlue: return "Light Blue"
+    case .darkBlue: return "Dark Blue"
+    case .midnight: return "Midnight"
+    case .darkGray: return "Dark Gray"
+    case .gray: return "Gray"
+    }
+  }
+
+  var color: Color {
+    Color(self.asset)
+  }
+
+  var a11y: String {
+    let key = asset.uppercased().replacingOccurrences(of: " ", with: "_")
+    return String(localized: .init(key))
+  }
+}
