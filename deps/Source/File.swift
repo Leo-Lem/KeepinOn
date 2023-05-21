41a5b85b404e5a67e3757668c2c148b6e2d01ba0
Leopold Lemmermann <leopold@lemmermann.com>
2023-05-21 12:15:20 +1200

    Added a deps package for declaring depenencies.

---
+++ deps/Source/File.swift
@@ -0,0 +1,3 @@
+// Created by Leopold Lemmermann on 21.05.2023.
+
+// TODO: get started on 'KeepinOnDependencies'
---
+++ deps/Test/Tests.swift
@@ -0,0 +1,10 @@
+// Created by Leopold Lemmermann on 21.05.2023.
+
+@testable import KeepinOnDependencies
+import XCTest
+
+final class KeepinOnDependenciesTests: XCTestCase {
+  
+  // TODO: write some tests
+  
+}
